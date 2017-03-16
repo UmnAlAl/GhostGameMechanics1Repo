6ac58d841fa4813d0db575a473a8ae0abed49355
Alexander <Alex170995@yandex.ru>
2017-03-16 16:54:56 +0300

    ARToolkit interaction

---
+++ Assets/CameraBehaviour/CapsulaCollisionBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsulaCollisionBehaviour : MonoBehaviour {
+
+
+	public void OnCollisionEnter(Collision collision) {
+		GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+	}
+}
