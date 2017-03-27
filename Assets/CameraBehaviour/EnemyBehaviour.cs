0bf9f3f8ec02e426d9a11e7b332da573383287f4
Alexander <Alex170995@yandex.ru>
2017-03-27 20:35:47 +0300

    Enemies

---
+++ Assets/CameraBehaviour/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public GameObject Followee;
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		//Followee = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Followee != null) {
+			transform.position = Vector3.MoveTowards (transform.position, Followee.transform.position, speed);
+		} else {
+			Debug.logger.Log (gameObject.name + ": my followee is null!!!");
+		}
+	}
+
+	public void OnCollisionEnter(Collision collision) {
+		DestroyObject (this.gameObject);
+	}
+
+	public void SetFollowee(GameObject flw) {
+		Followee = flw;
+	}
+
+}
---
+++ Assets/CameraBehaviour/EnemyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+	public GameObject EnemyPrefab;
+	public GameObject Followee;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (generateEnemies ());
+	}
+	
+	// Update is called once per frame
+	IEnumerator generateEnemies() {
+		while (true) {
+			GameObject newEnemy =  Instantiate (EnemyPrefab) as GameObject;
+			newEnemy.AddComponent<EnemyBehaviour> ();
+			EnemyBehaviour beh = newEnemy.GetComponent<EnemyBehaviour>();
+			beh.SetFollowee (Followee);
+
+			//Debug.logger.Log ("Followee real: " + Followee.ToString ());
+			//Debug.logger.Log ("Followee created: " + newEnemy.GetComponent<EnemyBehaviour>().Followee);
+
+			yield return new WaitForSecondsRealtime (3);
+		}
+	}
+}
